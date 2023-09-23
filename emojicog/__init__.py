5b7c2507a61c0ebdf60c3a29f770cbbcc3af353a
Tworch <116972095+Tworch@users.noreply.github.com>
2023-09-23 03:10:56 +0200

    Update __init__.py

--- emojicog/__init__.py
+++ emojicog/__init__.py
@@ -1,7 +1,4 @@
 from .emojicog import EmojiCog
-from redbot.core import commands
-from discord_slash import SlashCommand
 
-async def setup(bot: commands.Bot):
-    slash = SlashCommand(bot, sync_commands=True)  # Initializes slash commands
-    await bot.add_cog(EmojiCog(bot, slash))
+async def setup(bot):
+    await bot.add_cog(EmojiCog(bot))
